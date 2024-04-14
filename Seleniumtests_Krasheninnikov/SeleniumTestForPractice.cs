d1fdd12e1efa576267321d12b2f563f497fa8b43
Semyon Krasheninnikov <sa.krasheninnikov@yandex.ru>
2024-04-14 20:23:56 +0500

    Мой коммит

---
+++ Seleniumtests_Krasheninnikov/SeleniumTestForPractice.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Seleniumtests_Krasheninnikov;
+
+public class SeleniumTestForPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        
+        var options = new ChromeOptions();// опции хрома новый метод типо
+        options.AddArguments("--no-sandbox","--start-maximized", "--disable-extensions");//Здесь передаем аргументынастройк
+        
+        //зайти в хром (через вебдрайвер)
+        var driver = new ChromeDriver(options);
+        
+        // перейте в URL https://staff-testing.testkontur.ru/
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        Thread.Sleep(2000);
+        
+        //ввести логин sa.krasheninnikov@yandex.ru
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("sa.krasheninnikov@yandex.ru");
+        
+        //ввести пароль q1W2e!Semen 
+        var password = driver.FindElement(By.Id("Password"));
+        password.SendKeys("q1W2e!Semen");
+        
+        //нажать войти
+        var submit = driver.FindElement(By.Name("button"));
+        submit.Click();
+        
+        Thread.Sleep(2000);
+        //проверить что мы на нужной странице
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl  == "https://staff-testing.testkontur.ru/news");
+        
+        //закрыть хром и убить процесс драйвера
+        driver.Quit();  
+    }
+}   
