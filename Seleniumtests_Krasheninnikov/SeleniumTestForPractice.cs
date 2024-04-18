ff59365b22a75e7b3aef29c68e9b5d9f1e770584
Semyon Krasheninnikov <sa.krasheninnikov@yandex.ru>
2024-04-18 21:04:12 +0500

    Первая версия программы

--- Seleniumtests_Krasheninnikov/SeleniumTestForPractice.cs
+++ Seleniumtests_Krasheninnikov/SeleniumTestForPractice.cs
@@ -1,24 +1,37 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using FluentAssertions;
+using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace Seleniumtests_Krasheninnikov;
 
 public class SeleniumTestForPractice
 {
+    public ChromeDriver driver;
+    
     [Test]
     public void Authorization()
     {
+       
         //изменение через консоль еще изменение для gitextensions
         var options = new ChromeOptions();// опции хрома новый метод типо
-        options.AddArguments("--no-sandbox","--start-maximized", "--disable-extensions");//Здесь передаем аргументынастройк
-        
+        options.AddArguments("--no-sandbox","--window-size=1200x600", "--disable-extensions");//Здесь передаем аргументынастройк
+        //window-size=1200x600 start-maximized - для полного экрана
         //зайти в хром (через вебдрайвер)
-        var driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
+        
+        //неявное ожидание
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         
         // перейте в URL https://staff-testing.testkontur.ru/
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-        Thread.Sleep(2000);
+        
+        //ЯВНОЕ ОЖИДАНИЕ
+        // IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        //wait.Until(ExpectedConditions.ElementIsVisible(By.Id("Username")));
+        //Thread.Sleep(2000);
         
         //ввести логин sa.krasheninnikov@yandex.ru
         var login = driver.FindElement(By.Id("Username"));
@@ -30,14 +43,32 @@ public class SeleniumTestForPractice
         
         //нажать войти
         var submit = driver.FindElement(By.Name("button"));
-        submit.Click();
-        
-        Thread.Sleep(2000);
+        submit.Click(); 
+        //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+        //Thread.Sleep(2000);
         //проверить что мы на нужной странице
         var currentUrl = driver.Url;
-        Assert.That(currentUrl  == "https://staff-testing.testkontur.ru/news");
         
+        var currentURL = driver.FindElement(By.CssSelector("[data-tid='Title']"));
+        currentURL.Should().NotBeNull();
+        //Assert.That(currentUrl  == "https://staff-testing.testkontur.ru/news",
+        //"Current url = " + currentUrl + " а должен быть https://staff-testing.testkontur.ru/news");
+
         //закрыть хром и убить процесс драйвера
-        driver.Quit();  
+        //currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
+        
+        var menu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
+        menu.Click(); 
+        var communities = driver.FindElements(By.CssSelector("[data-tid='Community']"))[1];
+        communities.Click();
+        Thread.Sleep(20000);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        
+        driver.Quit();
+        Console.WriteLine("pivo");
     }
 }   
