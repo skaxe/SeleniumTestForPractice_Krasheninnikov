95b62494b1d664d19b94fca6deb576e4b62cb423
Semyon Krasheninnikov <sa.krasheninnikov@yandex.ru>
2024-04-27 03:07:57 +0500

    Исправил все недочеты

--- Seleniumtests_Krasheninnikov/SeleniumTestForPractice.cs
+++ Seleniumtests_Krasheninnikov/SeleniumTestForPractice.cs
@@ -1,10 +1,9 @@
-﻿﻿using FluentAssertions;
+﻿﻿using System.Drawing;
+ using FluentAssertions;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.DevTools.V121.FedCm;
-//using java.util.concurrent.TimeUnit;
-//using org.openqa.selenium.JavascriptExecutor;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 
@@ -13,56 +12,57 @@ namespace Seleniumtests_Krasheninnikov;
 
 public class SeleniumTestForPractice
 {
-    
-    public void Authorization_method(string args)
-    {
-        var options = new ChromeOptions();// опции хрома новый метод типо
-        options.AddArguments("--no-sandbox",args, "--disable-extensions");//Здесь передаем аргументынастройк
-        //window-size=1200x600 start-maximized - для полного экрана
-        //зайти в хром (через вебдрайвер)
-        driver = new ChromeDriver(options);
-        
-        //неявное ожидание
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(100);
-        
-        // перейте в URL https://staff-testing.testkontur.ru/
-        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-        
-        //ЯВНОЕ ОЖИДАНИЕ
-        // IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        //wait.Until(ExpectedConditions.ElementIsVisible(By.Id("Username")));
-        //Thread.Sleep(2000);
-        
-        //ввести логин sa.krasheninnikov@yandex.ru
-        var login = driver.FindElement(By.Id("Username"));
-        login.SendKeys("sa.krasheninnikov@yandex.ru");
-        
-        //ввести пароль q1W2e!Semen 
-        var password = driver.FindElement(By.Id("Password"));
-        password.SendKeys("q1W2e!Semen");
-        
-        //нажать войти
-        var submit = driver.FindElement(By.Name("button"));
-        submit.Click();
-        
-    }
     public ChromeDriver driver;
-    
+    [SetUp]
+    public void SetUP()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--disable-extensions");
+        driver = new ChromeDriver(options);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+    }
+    public void ExplicitExpectationByDatatid(string datatid)
+    {
+        IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[data-tid='"+datatid+"']")));
+    }
     [Test]
     public void Authorization()
-    {   
-        //Авторизируюсь
-        Authorization_method("--start-maximized");
-        var currentUrl = driver.Url;
-        //Смотрю, что есть нужный тег со страницы
+    {
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("sa.krasheninnikov@yandex.ru");
+        var password = driver.FindElement(By.Id("Password"));
+        password.SendKeys("q1W2e!Semen");
+        var submit = driver.FindElement(By.Name("button"));
+        submit.Click();
         var currentLocation = driver.FindElement(By.CssSelector("[data-tid='DateNews']"));
         currentLocation.Should().NotBeNull();
     }
     
     [Test]
+    public void SearchBar()
+    {   
+        Authorization();
+        //ищу профиль в поисковике
+        var search = driver.FindElement(By.CssSelector("[data-tid='SearchBar']"));
+        search.Click();
+        var inputsearch = driver.FindElement(By.CssSelector("[data-tid='SearchBar']")).FindElement(By.TagName("input"));
+        var profilename = "Семен1 Крашенинников1";
+        inputsearch.SendKeys(profilename);
+        var profile = driver.FindElement(By.CssSelector("[data-tid='ComboBoxMenu__item']"));
+        profile.Click();
+        //проверяю профиль
+        var profilepage = driver.FindElement(By.CssSelector("[data-tid='EmployeeName']")).Text;
+        profilepage.Should().Be(profilename);
+    }
+    
+    [Test]
     public void GoToCommunity()
     {
-        Authorization_method("--window-size=1000,600)");//разрешение чет не особо работает
+        Authorization();
+        driver.Manage().Window.Size = new Size(1000, 1000);
         //открываю сообщества кнопкой бокового меню
         var menu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
         menu.Click(); 
@@ -70,34 +70,34 @@ public class SeleniumTestForPractice
         communities.Click();
         //смотрю что мы оказались на странице сообщества
         var currentURL = driver.Url;
-        currentURL.Should().Be("https://staff-testing.testkontur.ru/communities", "Current url = " + currentURL);
+        currentURL.Should().Be("https://staff-testing.testkontur.ru/communities");
     }
-    
     
     [Test]
     public void EditProfile()
     {
-        Authorization_method("--start-maximized");
+        driver.Manage().Window.Maximize();
+        Authorization();
         //Иду в выпадающее меню
-        var drop_menu = driver.FindElement(By.CssSelector("[data-tid='PopupMenu__caption']")); //Avatar 
+        var drop_menu = driver.FindElement(By.CssSelector("[data-tid='PopupMenu__caption']")); 
         drop_menu.Click();
-        //тыкаю редактировать профиль
         var profile_edit = driver.FindElement(By.CssSelector("[data-tid='ProfileEdit']"));
         profile_edit.Click();
-        //смотрю, что мы на нужном Urle
+        //смотрю, что мы на нужной странице
         var currentURL = driver.Url;
-        currentURL.Should().Be("https://staff-testing.testkontur.ru/profile/settings/edit", "Current url = " + currentURL);
+        currentURL.Should().Be("https://staff-testing.testkontur.ru/profile/settings/edit");
     }
     
     [Test]
     public void CreateCommunity()
     {
-        Authorization_method("--start-maximized");
+        driver.Manage().Window.Maximize();
+        Authorization();
         //пошел в сообщества
         var communities = driver.FindElement(By.CssSelector("[data-tid='Community']"));
         communities.Click();
         //создаю сообщество
-        var createnewcommunity = driver.FindElement(By.XPath("/html/body/div/section/section[2]/section/div[2]/span/button"));
+        var createnewcommunity = driver.FindElement(By.CssSelector("[data-tid='PageHeader']")).FindElement(By.TagName("button"));
         createnewcommunity.Click();
         var namefield = driver.FindElement(By.CssSelector("[data-tid='Name']"));
         namefield.SendKeys("Название!123nameuniqal&^$#");
@@ -108,44 +108,36 @@ public class SeleniumTestForPractice
         //удаляю сообщество
         var deletebutton = driver.FindElement(By.CssSelector("[data-tid='DeleteButton']"));
         deletebutton.Click();
-        var deletebutton2 = driver.FindElements(By.CssSelector("[data-tid='DeleteButton']"))[1];
-        deletebutton2.Click();
+        var acceptdelete = driver.FindElements(By.CssSelector("[data-tid='DeleteButton']"))[1];
+        acceptdelete.Click();
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/communities");
-        //ЯВНОЕ ОЖИДАНИЕ
-        IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[data-tid='Item']")));
+        ExplicitExpectationByDatatid("CommunitiesCounter");
         //иду в сообщества, где я модератор
         var mycommunities = driver.FindElements(By.CssSelector("[data-tid='Item']"))[2];
         mycommunities.Click();
         //Проверяю, что сообществ где я модератор нету, значит оно успешно удалилось
-        var nocommunities = driver.FindElement(By.XPath("/html/body/div/section/section[2]/div[1]/div/section/div/div/div/h2"));
-        var cat = nocommunities.Text;
-        cat.Should().Be("Подходящих сообществ нет");
+        var nocommunities = driver.FindElement(By.CssSelector("[data-tid='Feed']")).FindElement(By.TagName("h2")).Text;
+        nocommunities.Should().Be("Подходящих сообществ нет");
     }
-
+    
     [Test]
-    public void AdditionalMail()//Не могу залезть в дату
+    public void AdditionalMail()
     {
-        EditProfile();
-        //Чищу поле Дополнительный и ввожу мейл
-        var cleartext = driver.FindElement(By.XPath(
-            "/html/body/div/section/section[2]/section[3]/div[2]/div[3]/label[2]/span[2]/input"));
-        cleartext.SendKeys("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
-        var text = driver.FindElement(By.XPath(
-            "/html/body/div/section/section[2]/section[3]/div[2]/div[3]/label[2]/span[2]/input"));
+        Authorization();
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/profile/settings/edit");
+        ExplicitExpectationByDatatid("FIO");
+        //Заполняю Дополнительный email
+        var additionalmail = driver.FindElement(By.CssSelector("[data-tid='AdditionalEmail']")).FindElement(By.CssSelector("[data-tid='Input']"));
+        additionalmail.SendKeys("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
         var inputtext = "randommail1234@mail.ru";
-        text.SendKeys(inputtext);
+        additionalmail.SendKeys(inputtext);
         //сохраняю
-        var saveopt = driver.FindElement(By.CssSelector("[data-tid='PageHeader']")).FindElement(By.TagName("button"));
-        saveopt.Click();
-        //иду в редактирвоание профиля
+        var save = driver.FindElement(By.CssSelector("[data-tid='PageHeader']")).FindElement(By.TagName("button"));
+        save.Click();
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/profile/settings/edit");
         //Смотрю что мейл тот который вписал
-        var readtext = driver.FindElement(By.XPath(
-            "/html/body/div/section/section[2]/section[3]/div[2]/div[3]/label[2]/span[2]/input")).GetAttribute("Value");
-        readtext.Should().Be(inputtext);
-        
-        
+        var readmail = driver.FindElement(By.CssSelector("[data-tid='AdditionalEmail']")).FindElement(By.TagName("input")).GetAttribute("Value");
+        readmail.Should().Be(inputtext);
     }
     
     [TearDown]
@@ -153,6 +145,5 @@ public class SeleniumTestForPractice
     {
         driver.Close();
         driver.Quit();
-        Console.WriteLine("pivo");
     }
 }   
